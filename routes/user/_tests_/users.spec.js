2419885531a94379edcaa4a907ccf90fa6f6a6c9
Jack <Jackw3434@gmail.com>
2019-10-15 18:07:30 +0100

    removed old db conection function

--- routes/user/_tests_/users.spec.js
+++ routes/user/_tests_/users.spec.js
@@ -1,33 +1,12 @@
 const frisby = require('frisby');
 let baseURL = 'http://localhost:8080/api/';
-let mongoose = require('mongoose');
 let User = require('../../../models/user');
-let connectionString = require('../../../config/connectionString').connectionString;
 let testHelper = require('../../../test/test-helper');
-
-const option = {
-    socketTimeoutMS: 30000,
-    keepAlive: true,
-    reconnectTries: 30000,
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
 
 describe('users test', function () {
 
     beforeAll(async (done) => {
         await testHelper.init;
-        // await mongoose.connect(connectionString, option, (err) => {
-        //     if (err) {
-        //         console.log(err);
-        //         console.log('Retrying Database Connection');
-        //         connectToMongoose();
-        //     } else {
-        //         console.log("Database Connected for tests");
-        //     }
-        // });
-
         return done();
     });
 
