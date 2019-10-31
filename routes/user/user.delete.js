6affa059a0e992056b5cfd859067619ed4a11f62
Jack <Jackw3434@gmail.com>
2019-10-31 22:22:52 +0000

    created user.delete by ID endpoint

---
+++ routes/user/user.delete.js
@@ -0,0 +1,16 @@
+let User = require('../../models/user');
+let auth = require('../../utils/auth');
+
+module.exports = function (router) {
+    router.route('/users/:id').delete(auth.required, function (req, res) {
+
+        User.deleteOne({ _id: req.params.id }, function (err, result) {
+
+            if (err || result.deletedCount == 0) {
+                return res.status(400).send('Unable to delete User for id ' + req.params.id);
+            }
+
+            res.status(200).json("User " + req.params.id + " has been deleted.");
+        })
+    });
+}
